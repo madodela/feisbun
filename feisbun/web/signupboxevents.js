a61acd6db805688b2f958b6aee53e1213faed55b
Omar Garcia <omarexperience@gmail.com>
2012-12-18 20:23:04 -0600

    Corrected Email Validation RegEx

--- feisbun/web/signupboxevents.js
+++ feisbun/web/signupboxevents.js
@@ -15,11 +15,10 @@ $(function(){
                 $(".emailmismatch").fadeOut();
             }
             if( ($("#username").data("default") == 1) || ($("#username").val() == "")){
-                debugger
                 $(".emptyfield").fadeIn();
                 return false;
             }
-            emailRegEx = /^[a-zA-Z][a-zA-Z0-9]*[.][a-zA-Z0-9]*@{1}feisbun[.]com/
+            emailRegEx = /^[a-zA-Z][a-zA-Z0-9]*[.]?[a-zA-Z0-9]*@{1}feisbun[.]com/
             email = $("#email").val();
             if( !emailRegEx.test(email) ){
                 $(".invalidemail").fadeIn();
